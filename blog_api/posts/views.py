1a209cb48400eac2c10c96254e3b6619df0969ce
tayfun karakavuz <tayfun.karakavuz@gmail.com>
2025-04-09 23:38:41 +0300

    page 129

--- blog_api/posts/views.py
+++ blog_api/posts/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
 class PostListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
